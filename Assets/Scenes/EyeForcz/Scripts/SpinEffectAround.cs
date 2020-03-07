fccebfc921fc5dbb6710f525ebe496291bf16093
Gamkedo <chris@gamkedo.com>
2020-03-07 01:06:44 -0800

    Hooked up EyeForcz clock :D

---
+++ Assets/Scenes/EyeForcz/Scripts/SpinEffectAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinEffectAround : MonoBehaviour
+{
+    public Transform pivot;
+    public float rate = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(pivot.position, pivot.forward, rate * Time.deltaTime);
+    }
+}
