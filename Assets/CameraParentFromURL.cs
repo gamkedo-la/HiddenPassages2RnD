c779cf04ff7f694f5e4a09d5d39dacb081e04668
Gamkedo <chris@gamkedo.com>
2020-03-21 16:09:56 -0700

    experiment to expose train clock camera position to JS script from webGL side

--- Assets/CameraParentFromURL.cs
+++ Assets/CameraParentFromURL.cs
@@ -8,18 +8,22 @@ public class CameraParentFromURL : MonoBehaviour
     public Transform parentInner;
     public bool outerCam = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(outerCam) {
+    public void trainCamSpot(int spotNum) {
+        if (spotNum == 1) {
             Camera.main.transform.SetParent(parentOuter);
-        }
-        else {
+            Camera.main.orthographic = true;
+        } else {
             Camera.main.transform.SetParent(parentInner);
             Camera.main.orthographic = false;
         }
         Camera.main.transform.localPosition = Vector3.zero;
         Camera.main.transform.localRotation = Quaternion.identity;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trainCamSpot(outerCam ? 1 : 0);
     }
 
     // Update is called once per frame
