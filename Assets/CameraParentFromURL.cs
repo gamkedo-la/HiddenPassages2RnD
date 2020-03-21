4010dace0bc3edcec1a89225748baf7fe070d6cf
Gamkedo <chris@gamkedo.com>
2020-03-21 15:39:19 -0700

    Script added to have toggle for viewing from inside the train, not yet tied to external URL parameter or updated in build

---
+++ Assets/CameraParentFromURL.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraParentFromURL : MonoBehaviour
+{
+    public Transform parentOuter;
+    public Transform parentInner;
+    public bool outerCam = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(outerCam) {
+            Camera.main.transform.SetParent(parentOuter);
+        }
+        else {
+            Camera.main.transform.SetParent(parentInner);
+            Camera.main.orthographic = false;
+        }
+        Camera.main.transform.localPosition = Vector3.zero;
+        Camera.main.transform.localRotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
