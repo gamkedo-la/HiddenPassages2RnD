13e1745ace2ffb25b34b65ebcbacbf9deb912b16
Gamkedo <chris@gamkedo.com>
2020-03-21 16:52:54 -0700

    Working on combining webGL scenes into one export, WIP (webGL unity scenes will be broken in this commit while I work on next step of it)

---
+++ Assets/CoreScripts/SceneSet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSet : MonoBehaviour
+{
+    public void SceneLoad(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void Start()
+    {
+        Debug.Log("scene switcher is loaded and ready");   
+    }
+}
