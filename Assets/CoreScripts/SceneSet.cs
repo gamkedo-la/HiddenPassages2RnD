bee2e7719d176e145bc9342643c00939aea9cb2b
Gamkedo <chris@gamkedo.com>
2020-03-21 17:41:31 -0700

    WIP test on scene switcher (webGL builds will not work yet)

--- Assets/CoreScripts/SceneSet.cs
+++ Assets/CoreScripts/SceneSet.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class SceneSet : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    static extern void OnScenesReady(); // in the javascript
     public void SceneLoad(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
 
     void Start()
     {
-        Debug.Log("scene switcher is loaded and ready");   
+        Debug.Log("scene switcher is loaded and ready");
+#if !UNITY_EDITOR && UNITY_WEBGL
+        OnScenesReady();
+#endif
     }
 }
