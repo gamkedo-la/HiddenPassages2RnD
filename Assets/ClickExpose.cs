492d3eb11be31da98dc7bb018856137bf326abde
Gamkedo <chris@gamkedo.com>
2020-03-28 23:30:42 -0700

    Implemented click exposre of EyeForcz crystal table models

---
+++ Assets/ClickExpose.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickExpose : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rhInfo;
+            if (Physics.Raycast(ray, out rhInfo)) {
+                Renderer rend = rhInfo.collider.GetComponent<Renderer>();
+                rend.enabled = true;
+            }
+        }
+    }
+}
