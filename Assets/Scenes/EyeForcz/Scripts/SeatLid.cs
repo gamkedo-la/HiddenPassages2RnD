1d6c33e7912370d60239d9a2f60d681b5be9cd36
Gamkedo <chris@gamkedo.com>
2020-04-12 11:19:33 -0700

    Toilet scene integrating EyeForcz model

---
+++ Assets/Scenes/EyeForcz/Scripts/SeatLid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatLid : MonoBehaviour
+{
+    public float offset = 0.0f;
+    void Update()
+    {
+        transform.rotation = Quaternion.AngleAxis(
+            -Mathf.Clamp01(Mathf.Cos(offset*Mathf.PI+Time.timeSinceLevelLoad*3.0f)) * 45.0f-90.0f, Vector3.right);
+    }
+}
