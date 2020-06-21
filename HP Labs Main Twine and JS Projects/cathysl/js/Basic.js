88ad725fd8bf2cc2c17b46250b85658fbf736fb7
CathySL <cathysanluis@users.noreply.github.com>
2020-06-21 22:10:20 +0800

    Setting up menu for basic and timed challenge

---
+++ HP Labs Main Twine and JS Projects/cathysl/js/Basic.js
@@ -0,0 +1,28 @@
+function basicGame(){
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+	loadImages();
+	randomizeGrid();
+	var framesPerSecond = 30;
+	setInterval(updateAll,1000/framesPerSecond);
+
+	canvas.addEventListener('mousedown',handleMouseClick);
+
+	canvas.addEventListener('mousemove',
+		function(evt) {
+			mousePos =  calculateMousePos(evt);
+		});
+}
+
+function imageLoadingDoneSoStartGame(){
+	var framesPerSecond = 30;
+	setInterval(updateAll,1000/framesPerSecond);
+}
+
+function updateAll(){
+	drawPuzzle();
+	drawMoving();
+	drawButtons();
+}
--- HP Labs Main Twine and JS Projects/cathysl/js/Main.js
+++ HP Labs Main Twine and JS Projects/cathysl/js/Main.js
@@ -1,10 +1,24 @@
+basicHighlight = false;
+timedHighlight = false;
 
 window.onload = function(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
-	colorRect(0,0, canvas.width,canvas.height, 'black');
-	loadImages();
+		colorRect(0,0, canvas.width,canvas.height, 'black');
+	    canvasContext.shadowBlur = 2;
+        canvasContext.shadowColor = '#3F3F74';
+
+        canvasContext.textAlign = 'center';
+        canvasContext.font = '20px Arial';
+        
+        canvasContext.fillStyle = basicHighlight ? '#5FCDE4' : '#516faf';
+        canvasContext.fillText("Basic Puzzle", canvas.width / 2, canvas.height / 2 + 20);
+
+        canvasContext.fillStyle = timedHighlight ? '#5FCDE4' : '#516faf';
+        canvasContext.fillText("Timed Challenge", canvas.width / 2, canvas.height / 2 + 50);
+}
+/*	loadImages();
 	randomizeGrid();
 	var framesPerSecond = 30;
 	setInterval(updateAll,1000/framesPerSecond);
@@ -27,3 +41,4 @@ function updateAll(){
 	drawMoving();
 	drawButtons();
 }
+*/
