c28d25a28c31732820facef8ec459e65016842d1
Gamkedo <chris@gamkedo.com>
2020-06-27 23:48:42 -0700

    Passcodes can now be typed

---
+++ Assets/FutureTypePrompt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FutureTypePrompt : MonoBehaviour
+{
+    InputField inputField;
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+        StartCoroutine(ForceFocus());
+    }
+
+    IEnumerator ForceFocus() {
+        while (true) {
+            inputField.Select();
+            inputField.ActivateInputField();
+             yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
