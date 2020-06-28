30ade478ea7e106e8871cfb50002ccaaeb521de6
Gamkedo <chris@gamkedo.com>
2020-06-28 00:03:42 -0700

    Password entry now matches against answer bank

--- Assets/FutureTypePrompt.cs
+++ Assets/FutureTypePrompt.cs
@@ -5,18 +5,36 @@ using UnityEngine.UI;
 
 public class FutureTypePrompt : MonoBehaviour
 {
+    public string[] answers;
     InputField inputField;
+    public Color answerColor;
+    public Color nonAnswerColor;
+
     void Start()
     {
         inputField = GetComponent<InputField>();
-        StartCoroutine(ForceFocus());
+        StartCoroutine(ForceFocusAndCheckAnswer());
     }
 
-    IEnumerator ForceFocus() {
+    IEnumerator ForceFocusAndCheckAnswer() {
+        bool anyMatch;
         while (true) {
             inputField.Select();
             inputField.ActivateInputField();
-             yield return new WaitForSeconds(0.1f);
+            anyMatch = false;
+            for(var i=0;i<answers.Length;i++) {
+                if (inputField.text == answers[i]) {
+                    anyMatch = true;
+                    break;
+                }
+            }
+            if(anyMatch) {
+                inputField.textComponent.color = answerColor;
+            } else {
+                inputField.textComponent.color = nonAnswerColor;
+            }
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
+
