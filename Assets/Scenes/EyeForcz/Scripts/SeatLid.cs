a136456ad15762c6a497ad312fe0efb7d03959ef
Gamkedo <chris@gamkedo.com>
2020-07-11 21:55:58 -0700

    Sounds integrated to bathroom scene

--- Assets/Scenes/EyeForcz/Scripts/SeatLid.cs
+++ Assets/Scenes/EyeForcz/Scripts/SeatLid.cs
@@ -6,9 +6,19 @@ public class SeatLid : MonoBehaviour
 {
     public float offset = 0.0f;
     public float stretchTilt = 1.0f;
+
+    public randToot soundMaker;
+
+    private float prevLevel;
+    private float clampLevel;
+
     void Update()
     {
-        transform.rotation = Quaternion.AngleAxis(
-            -Mathf.Clamp01(Mathf.Cos(offset*Mathf.PI+Time.timeSinceLevelLoad*1.9f)) * 45.0f* stretchTilt - 90.0f, Vector3.right);
+        clampLevel = -Mathf.Clamp01(Mathf.Cos(offset * Mathf.PI + Time.timeSinceLevelLoad * 1.9f)) * 45.0f * stretchTilt - 90.0f;
+        transform.rotation = Quaternion.AngleAxis(clampLevel, Vector3.right);
+        if(prevLevel==-90.0f && clampLevel < prevLevel) {
+            soundMaker.PlayRandomSound();
+        }
+        prevLevel = clampLevel;
     }
 }
---
+++ Assets/Scenes/EyeForcz/Scripts/randToot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randToot : MonoBehaviour
+{
+    public AudioClip[] soundList;
+
+    public void PlayRandomSound() {
+        PlayClipOn(soundList[Random.Range(0,soundList.Length)], transform.position, Random.Range(0.4f,0.7f), transform);
+    }
+
+    public void PlayClipOn(AudioClip clip, Vector3 pos, float atVol = 1.0f,
+                           Transform attachToParent = null) {
+        GameObject tempGO = new GameObject("TempAudio"); // create the temp object
+        tempGO.transform.position = pos; // set its position
+        if (attachToParent != null) {
+            tempGO.transform.parent = attachToParent.transform;
+        }
+        AudioSource aSource = tempGO.AddComponent<AudioSource>() as AudioSource; // add an audio source
+        aSource.clip = clip; // define the clip
+        aSource.volume = atVol;
+        aSource.pitch = Random.Range(0.7f, 1.4f);
+        // set other aSource properties here, if desired
+        aSource.Play(); // start the sound
+        Destroy(tempGO, clip.length / aSource.pitch); // destroy object after clip duration
+    }
+}
